5134e777f1457e1425e5b5edb8c2365bc1a3f27f
Zarchan <zachary.c.me@gmail.com>
2022-01-30 17:47:34 -0800

    .

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Form from "./components/Form";
 import {useEffect, useState} from "react";
 
+
 function App() {
   const url = "https://rhyming-backend.herokuapp.com/"
   const [rhymeWords, setRhymeWords] = React.useState(null)
 
   const get_rhyme = async (word, n) => {
-    const response = await fetch(`${url}/${word}`)
+    const response = await fetch(`${url}/`)
     setRhymeWords(response)
     //rhymeWords = "["dog","bog","sog","flog"]"
   }
